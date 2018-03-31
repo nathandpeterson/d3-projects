10d5069ab6a34823964626bf3632af94643a49ac
Nathan Daniel Peterson <natperson@gmail.com>
2018-03-31 14:52:02 -0700

    add linear scale to make height of chart proportional to data

--- bar_chart/main.js
+++ bar_chart/main.js
@@ -1,8 +1,8 @@
 
 
 const svg = d3.select('.chart').append('svg')
-.attr('width', 700)
-.attr('height', 500)
+.attr('width', '700')
+.attr('height', '500')
 
 const data = d3.json('data.json', (err, data) => {
   if(err) console.log(err)
@@ -10,11 +10,17 @@ const data = d3.json('data.json', (err, data) => {
   let bars = svg.selectAll('rect')
     .data(data)
 
+  const tallestBuilding = Math.max(...data.map(e => e.height)) 
+
+  const y = d3.scaleLinear()
+    .domain([0, tallestBuilding])
+    .range([0, 500])
+
   bars.enter()
     .append('rect')
     .attr('x', (d,i) => (i * 100) + 50 )
     .attr('y', 0) 
-    .attr('height', d => parseInt(d.height/7, 10) )
+    .attr('height', d => y(d.height))
     .attr('width', 60)
     .attr('fill', 'gray')
 })
